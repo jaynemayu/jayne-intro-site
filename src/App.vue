b0282c187f2a7090dd8c2a8d37b842144629a9f7
Jayne <jaynemayu@gmail.com>
2024-11-01 11:51:35 +0800

    layout update

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,21 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import SiteBar from './components/SideBar.vue';
+import Experience from './components/Experience.vue';
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container">
+    <SiteBar />
+    <Experience />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style lang="scss" scoped>
 @use "@/styles/variables" as *;
 
-.logo {
-  height: $logo-height;
-  padding: $logo-padding;
-  will-change: filter;
-  transition: filter 300ms;
-
-  &:hover {
-    filter: drop-shadow(0 0 2em $primary-color);
-  }
-}
-
-.logo.vue {
-  &:hover {
-    filter: drop-shadow(0 0 2em $secondary-color);
-  }
+.container {
+  display: flex;
+  height: 100vh; 
+  margin: 0; 
 }
 </style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="side-bar">
+    <div class="side-bar-photo-and-name">
+      <img src="@/assets/avatar.png" alt="Profile Avatar" class="avatar" />
+
+      <h1>林婕安</h1>
+      <p class="side-bar-photo-and-name-english-name">Jayne Lin</p>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.side-bar {
+  width: 20rem;
+  padding: 6rem 1.25rem; 
+
+  background-color: #e2e1e0; 
+
+  &-photo-and-name {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    letter-spacing: 0.5rem;
+
+    img {
+      border-radius: 50%;
+      width: 9rem;
+      border: 0.25rem solid #fff;
+    }
+
+    h1 {
+      margin: 0.5rem 0rem 0rem 0rem;
+    }
+
+    &-english-name {
+      margin: 0rem;
+      font-size: 0.75rem;
+    }
+  }
+
+
+}
+</style>
+
